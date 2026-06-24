e112a78e353f0b20a7bd24b8e582cc24eb2316fe
m1nhb1ee <minh.nt235976@sis.hust.edu.vn>
2026-06-24 14:44:18 +0700

    fix CORS

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,testserver', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.staticfiles',
